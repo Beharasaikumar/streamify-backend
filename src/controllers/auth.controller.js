1f98663872b019b06976f7ae87b5af38b8d1b694
Beharasaikumar <beharasaikumar1@gmail.com>
2026-07-20 15:00:13 -0400

    improvements

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -97,8 +97,16 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt");
-  res.status(200).json({ success: true, message: "Logout successful" });
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logout successful",
+  });
 }
 
 export async function onboard(req, res) {
